a280220d49d7714b9050ebbe006ef14dc85b663b
Ben Frame <brewerytattooer@gmail.com>
2024-03-28 08:59:36 -0400

    Wip cleaning up code

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -32,7 +32,7 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+
       <div className="graphs-section">
         <div className="image-container">
           <img src={GrantRatesByOfficeImg} alt="Grant rates by office" />
@@ -98,7 +98,7 @@ function RenderLandingPage(props) {
 
         <div className="body-bottom-section">
           <div>
-            <h2 className="sub-head-bottom-section">36%</h2>
+            <h2 className="info-head-bottom-section">36%</h2>
             <p>
               By the end of the Trump administration, the average asylum office
               grant rate had fallen 36 percent from an average of 44 percent in
@@ -106,14 +106,14 @@ function RenderLandingPage(props) {
             </p>
           </div>
           <div>
-            <h2 className="sub-head-bottom-section">5%</h2>
+            <h2 className="info-head-bottom-section">5%</h2>
             <p>
               The New York asylum office grant rate dropped to 5 percent in
               fiscal year 2020.
             </p>
           </div>
           <div>
-            <h2 className="sub-head-bottom-section">6x Lower</h2>
+            <h2 className="info-head-bottom-section">6x Lower</h2>
             <p>
               Between fiscal year 2017 and 2020, the New York asylum offices's
               average grant rate was six times lower than San Francisco asylum
